5be87d5fa8580eede0c837a77e2737b5c390b5bf
buianhduc <buianhduc.popodoo2@gmail.com>
2021-03-01 09:57:16 +0700

    adjust abtChallenge.html

--- js/abtChallenge.js
+++ js/abtChallenge.js
@@ -2,9 +2,13 @@ $(".fa-heart").click(function() {
     if($(this).hasClass("far")){
         $(this).removeClass("far");
         $(this).addClass("fas");
+        let peopleLiked = String(parseInt($(this).siblings(".peopleLiked").text()) + 1);
+        $(this).siblings(".peopleLiked").html(peopleLiked);
     }
     else{
         $(this).removeClass("fas");
         $(this).addClass("far");
+        let peopleLiked = String(parseInt($(this).siblings(".peopleLiked").text()) - 1);
+        $(this).siblings(".peopleLiked").html(peopleLiked);
     }
 })
